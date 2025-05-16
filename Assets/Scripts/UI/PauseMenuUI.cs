9dd1451ebc97341e348beb1836a13d3294c8d83b
김태현 <th2252@gmail.com>
2025-05-16 16:09:35 +0900

    일시정지 메뉴 작동안하는 것 수정

--- Assets/Scripts/UI/PauseMenuUI.cs
+++ Assets/Scripts/UI/PauseMenuUI.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 /// <summary>
 /// 일시정지 메뉴
@@ -15,6 +14,8 @@ public class PauseMenuUI : MonoBehaviour
     private void Start()
     {
         InputManager.Instance.AM_UiInGame.FindAction("Pause").performed += OnPerformedPauseButton;
+
+        ClosePauseMenu();
     }
 
     private void OnPerformedPauseButton(UnityEngine.InputSystem.InputAction.CallbackContext obj)
@@ -31,6 +32,7 @@ public class PauseMenuUI : MonoBehaviour
 
     public void OpenPauseMenu()
     {
+        isPaused = true;
         Time.timeScale = 0f;
         uiObject.SetActive(true);
         // TODO: 일시정지 메뉴 열기 전 상태(걷기/기어오르기 등)을 저장해뒀다가 일시정지 해제되면 복구
@@ -39,6 +41,7 @@ public class PauseMenuUI : MonoBehaviour
 
     public void ClosePauseMenu()
     {
+        isPaused = false;
         Time.timeScale = 1f;
         uiObject.SetActive(false);
         InputManager.Instance.SetUiInputState(UiState.IN_GAME);
