0af4c977f8985382ece28c4ffc8cd55fe72139f9
김태현 <th2252@gmail.com>
2025-11-27 16:01:25 +0900

    악어 돌진 공격에 씨앗 생성 기능 추가

---
+++ Assets/Scripts/Monster/AITask/Task_GA_Tackle_Crocodile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class Task_GA_Tackle_Crocodile : Task_GA_Tackle
+{
+    [Title("공격 아이템 스폰 관련")]
+    [SerializeField] private GameObject attackItemPrefab;
+    [SerializeField] private Transform attackItemSpawnPos;
+    [SerializeField] private float attackItemPopVertical = 100f;
+    [SerializeField] private float attackItemPopHorizontal = 1f;
+    [SerializeField] private float attackItemSpawnDelay = 0.1f;
+    
+    protected override void OnActiveBegin()
+    {
+        base.OnActiveBegin();
+        StartCoroutine(SpawnAttackItem());
+    }
+
+    private IEnumerator SpawnAttackItem()
+    {
+        if (!attackItemPrefab)
+            yield break;
+        
+        yield return new WaitForSeconds(attackItemSpawnDelay);
+        var instance = Instantiate(attackItemPrefab, attackItemSpawnPos.position, Quaternion.identity);
+        
+        // 공격 아이템에 살짝 튀어오르는 연출
+        var popVector = new Vector2(attackItemPopVertical,
+            Random.Range(-attackItemPopHorizontal, attackItemPopHorizontal));
+        instance.GetComponent<Rigidbody2D>().AddForce(popVector);
+    }
+}
