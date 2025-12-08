27bbd646ca89739ae7fde3df0bc1793964528655
김태현 <th2252@gmail.com>
2025-12-08 21:05:03 +0900

    피라냐 연출 보강
    수면 위로 튀어오르지 않았을 때 숨김처리 추가
    튀어오르기 시작과 끝에 물 튀는 파티클 연출 추가

--- Assets/Scripts/Monster/AITask/Task_A_Piranha.cs
+++ Assets/Scripts/Monster/AITask/Task_A_Piranha.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using Panda;
 using DG.Tweening;
@@ -7,36 +8,65 @@ using DG.Tweening;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Task_A_Piranha : MonoBehaviour
 {
+    [SerializeField] private GameObject piranhaVisual;
+    [SerializeField] private ParticleSystem[] waterSplashParticles;
+    
     [SerializeField] float breachHeight = 3f;     // 튀어오르는 높이
     [SerializeField] float breachUpTime = 1f;
     [SerializeField] float breachDownTime = 0.7f;
     [SerializeField] float timeBetweenBreach = 1.0f;
     [SerializeField] float startDelay = 0;       // 딜레이를 줘서 다른 피라냐들과 같이 파도타기 구현 가능
+    
+    private float _originHeight;
 
-    float originHeight;
-
-    private new Rigidbody2D rigidbody;
+    private Rigidbody2D _rigidbody;
 
     private void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
-        originHeight = transform.position.y;
-        DOTween.Sequence().AppendInterval(startDelay).AppendCallback(
-            () =>
-            {
-                DOTween.Sequence()
-                .Append(
-                    rigidbody.DOMoveY(transform.position.y + breachHeight, breachUpTime)
-                        .SetEase(Ease.OutCubic))
-                .Insert(0, transform.DORotate(new Vector3(0, 0, 0), 0.2f))
-                .Append(
-                    rigidbody.DOMoveY(originHeight, breachDownTime)
-                        .SetEase(Ease.InQuad))
-                .Insert(breachUpTime, transform.DORotate(new Vector3(0, 0, 180), 0.2f))
-                .AppendInterval(timeBetweenBreach)
-                .SetLoops(-1);
-            }
-        );
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _originHeight = transform.position.y;
+        
+        StartTween().Forget();
+    }
+
+    private async UniTaskVoid StartTween()
+    {
+        await UniTask.WaitForSeconds(startDelay);
+        DOTween.Sequence()
+            .AppendCallback(() => { OnBreachStart(); })
+            .Append(
+                _rigidbody.DOMoveY(transform.position.y + breachHeight, breachUpTime)
+                    .SetEase(Ease.OutCubic))
+            .Insert(0, transform.DORotate(new Vector3(0, 0, 0), 0.2f))
+            .Append(
+                _rigidbody.DOMoveY(_originHeight, breachDownTime)
+                    .SetEase(Ease.InQuad))
+            .Insert(breachUpTime, transform.DORotate(new Vector3(0, 0, 180), 0.2f))
+            .AppendCallback(() => { OnBreachEnd(); })
+            .AppendInterval(timeBetweenBreach)
+            .SetLoops(-1);
+    }
+    
+    private void OnBreachStart()
+    {
+        piranhaVisual.SetActive(true);
+        foreach(var p in waterSplashParticles)
+        {
+            if(p.isPlaying)
+                p.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            p.Play();
+        }
+    }
+
+    private void OnBreachEnd()
+    {
+        piranhaVisual.SetActive(false);
+        foreach(var p in waterSplashParticles)
+        {
+            if(p.isPlaying)
+                p.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            p.Play();
+        }
     }
 
     private void OnDrawGizmosSelected()
@@ -45,7 +75,7 @@ public class Task_A_Piranha : MonoBehaviour
         if(Application.isPlaying)
         {
             Vector3 position = transform.position;
-            position.y = originHeight;
+            position.y = _originHeight;
             Gizmos.DrawLine(position, position + new Vector3(0, breachHeight, 0));
         }
         else
