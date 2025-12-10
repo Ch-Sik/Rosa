e0366f65b9b30b1bc81480227c7322c7209350d3
김태현 <th2252@gmail.com>
2025-12-10 21:19:26 +0900

    발사꽃 에임 따라가는 것 조정
    사정거리에 처음으로 플레이어가 들어왔을 때 한정으로 에임 방향으로 고개를 빠르게 이동하도록 개선
    고정형 발사꽃이 설정한 에임으로 고개를 빠르게 이동하도록 개선

--- Assets/Scripts/Monster/Animation/AnimBonePositionHandler.cs
+++ Assets/Scripts/Monster/Animation/AnimBonePositionHandler.cs
@@ -22,7 +22,8 @@ public class AnimBonePositionHandler : MonoBehaviour
     [ShowIf("useAimMotion"), Tooltip("Aim 따라가기 속도")]
     [SerializeField] private float aimFollowSpeed = 0.3f;
 
-    private Vector2 currentAimPos;
+    private Vector2 _currentAimPos;
+    private bool _trackingEnemy;
 
 
     // Start is called before the first frame update
@@ -38,34 +39,52 @@ public class AnimBonePositionHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(useAimMotion)
+        if (!useAimMotion) return;
+        
+        bool updateAimPosition = true;
+
+        // 에임을 업데이트해도 되는지 우선 체크
+        if (stopAimWhenAttack)
         {
-            bool updateAimPosition = true;
-
-            // 에임을 업데이트해도 되는지 우선 체크
-            if (stopAimWhenAttack)
-            {
-                int attackState;
-                blackboard.TryGet(BBK.AttackState, out attackState);
-                // 공격 진행중일 때 조준 따라가기 멈춤 (후딜은 포함 안됨)
-                if (attackState == 1 || attackState == 2) updateAimPosition = false;    
-            }
-
-            // 에임을 업데이트해도 된다고 판단되면 에임 위치 정보 가져오기
-            if (updateAimPosition)
-            {
-                GameObject enemy;
-                blackboard.TryGet(BBK.Enemy, out enemy);
-                if (enemy != null)
-                {
-                    // currentAimPos = Vector2.Lerp(currentAimPos, enemy.transform.position, 0.1f);
-                    currentAimPos = Vector2.MoveTowards(currentAimPos, enemy.transform.position, aimFollowSpeed);
-                }
-            }
-
-            // 실제 본 위치 업데이트 수행
-            targetPortrait.SetBonePosition(enemyAimBoneName, currentAimPos, Space.World);
+            int attackState;
+            blackboard.TryGet(BBK.AttackState, out attackState);
+            // 공격 진행중일 때 조준 따라가기 멈춤 (후딜은 포함 안됨)
+            if (attackState == 1 || attackState == 2) updateAimPosition = false;    
         }
+        if (!updateAimPosition) return;
+            
+        // 에임을 업데이트해도 된다고 판단되면 에임 위치 정보 가져오기
+        GameObject enemy;
+        blackboard.TryGet(BBK.Enemy, out enemy);
+        if (!enemy)
+        {
+            _trackingEnemy = false;
+            return;
+        }
+
+        if (!_trackingEnemy)
+        {
+            // 플레이어를 인식한 첫 프레임에는 Aim 본을 즉시 이동
+            JumpAim(enemy.transform.position);
+        }
+        else
+        {
+            TweenAim(enemy.transform.position);
+        }
+        _trackingEnemy = true;
+    }
+
+    private void JumpAim(Vector3 targetPos)
+    {
+        _currentAimPos = targetPos;
+        targetPortrait.SetBonePosition(enemyAimBoneName, _currentAimPos, Space.World);
+    }
+
+    private void TweenAim(Vector3 targetPos)
+    {
+        // Aim 본을 천천히 이동
+        _currentAimPos = Vector2.MoveTowards(_currentAimPos, targetPos, aimFollowSpeed);
+        targetPortrait.SetBonePosition(enemyAimBoneName, _currentAimPos, Space.World);
     }
 
     private void OnDrawGizmos()
@@ -73,7 +92,7 @@ public class AnimBonePositionHandler : MonoBehaviour
         if (Application.isPlaying)
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(currentAimPos, 0.2f);
+            Gizmos.DrawWireSphere(_currentAimPos, 0.2f);
         }
     }
 }
