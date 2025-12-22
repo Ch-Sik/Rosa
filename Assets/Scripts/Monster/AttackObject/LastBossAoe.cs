0a4b01f5328031b6db5cb25110dc14e883e7d09a
김태현 <th2252@gmail.com>
2025-12-23 00:33:13 +0900

    최종보스 구체 공격 이펙트 개선
    시전 준비단계에서 불투명도를 낮춰 공격 판정 활성화된 것과 확실히 구분되게 개선

--- Assets/Scripts/Monster/AttackObject/LastBossAoe.cs
+++ Assets/Scripts/Monster/AttackObject/LastBossAoe.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 
 public class LastBossAoe : MonoBehaviour
@@ -8,13 +9,20 @@ public class LastBossAoe : MonoBehaviour
     [SerializeField] private Collider2D col;
     [SerializeField] private Animator anim;
     [SerializeField] private float activeDuration;
-
-    private int stateNameHash;
+    [SerializeField] private ParticleSystem[] particles;
+    
+    private int _stateNameHash;
+    private Sequence _colorTween = null;
     
     private void Start()
     {
         col.enabled = false;
-        stateNameHash = Animator.StringToHash("State");
+        _stateNameHash = Animator.StringToHash("State");
+        foreach (var p in particles)
+        {
+            var shape = p.shape;
+            shape.radius *= transform.localScale.x;
+        }
     }
     
     public void Activate()
@@ -25,9 +33,12 @@ public class LastBossAoe : MonoBehaviour
     private async UniTaskVoid ActivateInternal()
     {
         col.enabled = true;
-        anim.SetInteger(stateNameHash, 1);
+        anim.SetInteger(_stateNameHash, 1);
+        foreach(var p in particles)
+            p.Play();
+        
         await UniTask.WaitForSeconds(activeDuration);
-        anim.SetInteger(stateNameHash, 0);
+        anim.SetInteger(_stateNameHash, 0);
         col.enabled = false;
         await UniTask.WaitForSeconds(2f);
         Destroy(gameObject);
