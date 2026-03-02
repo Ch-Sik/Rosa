c2a4a5a06cd1a3d2a8d2cf384c275bde464a6d2f
김태현 <th2252@gmail.com>
2026-03-03 01:47:14 +0900

    베리 2번 습득 가능한 것 수정

--- Assets/Scripts/Items/BerryEffect.cs
+++ Assets/Scripts/Items/BerryEffect.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class BerryEffect : MonoBehaviour
 {
     [SerializeField] float _healAmount;
+    [SerializeField] private Collider2D col;
 
     public void OnGetBerry()
     {
         Debug.Log("베리 습득");
         PlayerRef.Instance.state.Heal(_healAmount);
+        // 2번 획득 가능한 거 방지
+        if (col)
+            col.enabled = false;
     }
 }
